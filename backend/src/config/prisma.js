9bc316604afe43e328837b7253f3ea945d537339
Tincho99 <martinsander99@outlook.com>
2026-09-02 13:54:05 -0300

    alta de tipos de cancha y modifica config de Prisma

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client.js";
+import pkg from "@prisma/client";
+
+const { PrismaClient } = pkg;
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
---
+++ backend/src/services/crearTipoCancha.js
@@ -0,0 +1,31 @@
+import { prisma } from "../config/prisma.js";
+
+export async function crearTipoCancha({
+  descripcion,
+} = {}) {
+
+  // Comprueba si hay parametro
+  if (descripcion == null || typeof descripcion !== "string" ||
+    descripcion.trim() === "") {
+    throw new Error("Error, descripcion no valida");
+  }
+
+  // Comprueba que la descripcion no existe
+  const tipoExistente = await prisma.tipoCancha.findUnique({
+    where: {
+      descripcion: descripcion.trim(),
+    },
+  });
+  if (tipoExistente) {
+    throw new Error("El tipo de cancha ya existe");
+  }
+
+  // Crea el tipo de cancha
+  const tipoCancha = await prisma.tipoCancha.create({
+    data: {
+      descripcion: descripcion.trim(),
+    },
+  });
+
+  return tipoCancha;
+}
